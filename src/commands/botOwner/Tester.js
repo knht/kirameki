961e6355ca6245cb27b9cceafd88042f516c9d4a
Riya <kato.hana@outlook.jp>
2019-03-06 20:00:03 +0100

    Changes to ignore file

--- src/commands/botOwner/Tester.js
+++ src/commands/botOwner/Tester.js
@@ -11,6 +11,11 @@ class Tester {
     async execute(message, kirCore) {
         const [command, args] = KiramekiHelper.tailedArgs(message.content, ' ', 1);
 
+        if (message.mentions.length) {
+            message.channel.createMessage("yes");
+        } else {
+            message.channel.createMessage("nope");
+        }
     }
 }
 
