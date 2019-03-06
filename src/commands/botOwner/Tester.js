2059620ce80a4c4d638b5f0c2f8fd0eac4019436
Riya <kato.hana@outlook.jp>
2019-03-06 17:20:00 +0100

    Indentation fix

--- src/commands/botOwner/Tester.js
+++ src/commands/botOwner/Tester.js
@@ -1,16 +1,16 @@
-const KiramekiHelper = require('../../KiramekiHelper');
-const KiramekiConfig = require('../../../config/KiramekiConfig');
-const fetch             = require('node-fetch');
+const KiramekiHelper 	= require('../../KiramekiHelper');
+const KiramekiConfig 	= require('../../../config/KiramekiConfig');
+const fetch 			= require('node-fetch');
 
 class Tester {
 	constructor() {
-        this.name = 'tester';
-        this.owner = true;
+		this.name = 'tester';
+		this.owner = true;
 	}
 
 	async execute(message, kirCore) {
 		const [command, args] = KiramekiHelper.tailedArgs(message.content, ' ', 1);
-		
+
 	}
 }
 
