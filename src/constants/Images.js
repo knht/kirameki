7f2f885f70d1ab65d61d09094fac177396db9a23
Riya <kato.hana@outlook.jp>
2019-03-19 16:18:47 +0100

    Added profile card constants

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -1,5 +1,11 @@
 module.exports = {
     OSU_LOGO: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Osulogo.png/286px-Osulogo.png',
     OSU_PIPPI_SAD: 'https://i.imgur.com/Wa00gFg.png',
-    DEFAULT_DISCORD: 'https://img.kirameki.one/tJIX0aeZ.png'
+    DEFAULT_DISCORD: 'https://img.kirameki.one/tJIX0aeZ.png',
+    PROFILE_CARD: {
+        GURU_BADGE: 'https://img.kirameki.one/gBd284Hs.png',
+        PEACEKEEPER_BADGE: 'https://img.kirameki.one/GGaghPuT.png',
+        OWNER_BADGE: 'https://img.kirameki.one/8A5fZRMN.png',
+        OHK_BADGE: 'https://img.kirameki.one/gB9anVUA.png'
+    }
 };
