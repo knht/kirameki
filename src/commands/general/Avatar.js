00d1c370dc8db69a4b842dafed4ef5a29355025c
Riya <kato.hana@outlook.jp>
2019-04-01 16:29:45 +0200

    Finished command

--- src/commands/general/Avatar.js
+++ src/commands/general/Avatar.js
@@ -16,17 +16,35 @@ class Avatar {
 
     async execute(message, kirCore) {
         const [command, target] = KiramekiHelper.tailedArgs(message.content, ' ', 1);
-        const avatarEmbed       = new KiramekiHelper.Embed().setColor('DEFAULT').setTitle(`${KiramekiHelper.getUserTag(message.author)}'s Avatar`);
+        const avatarEmbed       = new KiramekiHelper.Embed().setColor('DEFAULT');
 
         if (!target) {
+            avatarEmbed.setTitle(`${KiramekiHelper.getUserTag(message.author)}'s Avatar`);
             avatarEmbed.setImage(message.author.dynamicAvatarURL('jpg', 1024));
             avatarEmbed.setDescription(`[Click here to expand in your browser](${message.author.dynamicAvatarURL('jpg', 1024)})`);
         } else if (target.toLowerCase() === 'guild' || target.toLowerCase() === 'server') {
+            avatarEmbed.setTitle(`${message.channel.guild.name}'s Avatar`)
             avatarEmbed.setImage(message.channel.guild.dynamicIconURL('jpg', 1024));
             avatarEmbed.setDescription(`[Click here to expand in your browser](${message.channel.guild.dynamicIconURL('jpg', 1024)})`);
         } else {
-            // TODO: Implement user mentions
+            const mentionedUser = message.mentions[0] || message.channel.guild.members.find(member => member.username.toLowerCase() === target.toLowerCase());
+            
+            if (!mentionedUser) {
+                return message.channel.createEmbed(new KiramekiHelper.Embed()
+                    .setColor('RED')
+                    .setTitle('Couldn\'t find the user specified. Maybe try a mention?')
+                );
+            }
+
+            const taggedUser = (!mentionedUser.user) ? mentionedUser : mentionedUser.user;
+
+            avatarEmbed.setTitle(`${KiramekiHelper.getUserTag(taggedUser)}'s Avatar`);
+            avatarEmbed.setImage(taggedUser.dynamicAvatarURL('jpg', 1024));
+            avatarEmbed.setDescription(`[Click here to expand in your browser](${taggedUser.dynamicAvatarURL('jpg', 1024)})`);
         }
+
+        message.channel.createEmbed(avatarEmbed);
+        KiramekiHelper.log(KiramekiHelper.LogLevel.COMMAND, 'AVATAR', `${KiramekiHelper.userLogCompiler(message.author)} used the avatar command.`);
     }
 }
 
