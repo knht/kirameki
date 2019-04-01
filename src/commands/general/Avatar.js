ba31d9a1ac1a2243c7514bc50b9c9028ec47ee89
Riya <kato.hana@outlook.jp>
2019-04-01 16:10:02 +0200

    Added nearly all avatar functionality

--- src/commands/general/Avatar.js
+++ src/commands/general/Avatar.js
@@ -7,7 +7,7 @@ class Avatar {
         this.owner = false;
         this.aliases = ['guildicon', 'useravatar', 'userimage'];
         this.help = {
-            message: 'Request the high resolution avatar of a specified user, yourself, or the guild by specifying a target of your choice. Leaving the target empty will show your own avatar instead.',
+            message: 'Request the avatar of a specified user, yourself, or the guild by specifying a target of your choice. Leaving the target empty will show your own avatar instead.',
             usage: 'avatar [target]',
             example: ['avatar', 'avatar @Riya#0001', 'avatar guild', 'avatar server'],
             inline: false
@@ -16,8 +16,17 @@ class Avatar {
 
     async execute(message, kirCore) {
         const [command, target] = KiramekiHelper.tailedArgs(message.content, ' ', 1);
+        const avatarEmbed       = new KiramekiHelper.Embed().setColor('DEFAULT').setTitle(`${KiramekiHelper.getUserTag(message.author)}'s Avatar`);
 
-        // TODO: create command
+        if (!target) {
+            avatarEmbed.setImage(message.author.dynamicAvatarURL('jpg', 1024));
+            avatarEmbed.setDescription(`[Click here to expand in your browser](${message.author.dynamicAvatarURL('jpg', 1024)})`);
+        } else if (target.toLowerCase() === 'guild' || target.toLowerCase() === 'server') {
+            avatarEmbed.setImage(message.channel.guild.dynamicIconURL('jpg', 1024));
+            avatarEmbed.setDescription(`[Click here to expand in your browser](${message.channel.guild.dynamicIconURL('jpg', 1024)})`);
+        } else {
+            // TODO: Implement user mentions
+        }
     }
 }
 
