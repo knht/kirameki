b3218546bf696dd8c8bba33374792463c2e83994
Riya <kato.hana@outlook.jp>
2019-04-11 18:18:13 +0200

    Added magic conch shell command

---
+++ src/commands/memes/MagicConchShell.js
@@ -0,0 +1,39 @@
+const KiramekiHelper = require('../../KiramekiHelper');
+
+class MagicConchShell {
+    constructor() {
+        this.name = 'mcs';
+        this.aliases = ['magicshell', 'conchshell', 'conch', '8ball'];
+        this.category = KiramekiHelper.categories.MEMES;
+        this.help = {
+            message: 'Ask the Magic Conch Shell from SpongeBob Squarepants a question and receive true enlightenment. The shell is always right and predicts the future 100% reliably. *cough*',
+            usage: 'mcs <question>',
+            example: 'mcs Can I have something to eat?',
+            inline: true
+        }
+    }
+
+    async execute(message, kirCore) {
+        const [command, question] = KiramekiHelper.tailedArgs(message.content, ' ', 1);
+        const predictions = KiramekiHelper.other.MAGIC_CONCH_SHELL.PREDICTIONS;
+        const prediction = predictions[KiramekiHelper.randomIntFromInterval(0, predictions.length - 1)];
+
+        if (!question) {
+            return message.channel.createEmbed(KiramekiHelper.generateHelpEmbed(this.help, this.help.inline));
+        }
+
+        message.channel.createEmbed(new KiramekiHelper.Embed()
+            .setColor('DEFAULT')
+            .setDescription('The magic conch shell has spoken!')
+            .setAuthor('Magic Conch Shell', KiramekiHelper.images.MAGIC_CONCH_SHELL.ICON)
+            .setThumbnail(KiramekiHelper.images.MAGIC_CONCH_SHELL.THUMBNAIL)
+            .addField('Question', question, false)
+            .addField('Answer', `***${prediction}***`, false)
+            .setFooter(`The magic conch shell was asked by: ${message.author.username}`, message.author.dynamicAvatarURL('jpg', 128))
+        );
+
+        KiramekiHelper.log(KiramekiHelper.LogLevel.COMMAND, 'MAGIC CONCH SHELL', `${KiramekiHelper.userLogCompiler(message.author)} used the ${this.name} command.`);
+    }
+}
+
+module.exports = new MagicConchShell();
