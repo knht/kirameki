b96da4ad4bafed0eb666dc13f20cf1b44ea024e1
Riya <kato.hana@outlook.jp>
2019-05-09 10:06:14 +0200

    Finished command

--- src/commands/botOwner/SetStatus.js
+++ src/commands/botOwner/SetStatus.js
@@ -1,27 +1,57 @@
 const KiramekiHelper = require('../../KiramekiHelper');
+const KiramekiConfig = require('../../../config/KiramekiConfig');
 
 class SetStatus {
     constructor() {
-        this.name = '';
-        this.aliases = [''];
-        this.permissions = [''];
-        this.userPermissions = [''];
+        this.name = 'setstatus';
+        this.aliases = ['addstatus'];
         this.category = KiramekiHelper.categories.OWNER;
-        this.owner = false;
-        this.nsfw = false;
-        this.cooldown = 2;
+        this.owner = true;
+        this.cooldown = 3;
         this.help = {
-            message: '',
-            usage: '',
-            example: '',
+            message: `Set the status for the \`${KiramekiConfig.prefix}\` command.`,
+            usage: 'setstatus <status> <statusMessage>',
+            example: 'setstatus 0 Fully operational',
             inline: false
         }
     }
 
     async execute(message, kirCore) {
-        const [command, args] = KiramekiHelper.tailedArgs(message.content, ' ', 1);
+        const [command, status, description] = KiramekiHelper.tailedArgs(message.content, ' ', 2);
+        const validStati = ['0', '1', '2'];
 
-        KiramekiHelper.log(KiramekiHelper.LogLevel.COMMAND, '', `${KiramekiHelper.userLogCompiler(message.author)} used the ${this.name} command.`);
+        if (!status || !validStati.includes(status)) {
+            return message.channel.createEmbed(new KiramekiHelper.Embed()
+                .setColor("RED")
+                .setTitle("Please specify a valid status")
+                .setDescription(
+                    "`0` Fully Operational\n" + 
+                    "`1` Restricted Operation\n" + 
+                    "`2` Not Operational"
+                )
+            );
+        }
+
+        if (!description) return message.channel.createEmbed(new KiramekiHelper.Embed().setColor("RED").setTitle("Please specify a valid description!"));
+
+        try {
+            const insertion = await Util.preparedAsyncMysqlQuery(
+                kirAPI_DB, 
+                'INSERT INTO status (id, status, date, description) VALUES (NULL, ?, ?, ?);', 
+                [status, Math.floor(Date.now() / 1000), description]
+            );
+
+            message.channel.createEmbed(new KiramekiHelper.Embed()
+                .setColor("GREEN")
+                .setTitle("Status Updater")
+                .setDescription("The status has been updated successfully!")
+            );
+
+            KiramekiHelper.log(KiramekiHelper.LogLevel.COMMAND, 'SET STATUS', `${KiramekiHelper.userLogCompiler(message.author)} used the ${this.name} command.`);
+        } catch (mysqlError) {
+            message.channel.createEmbed(new KiramekiHelper.Embed().setColor("RED").setTitle("Something went wrong. Check logs for more information!"));
+            KiramekiHelper.log(KiramekiHelper.LogLevel.ERROR, 'SET STATUS MYSQL ERROR', mysqlError);
+        }
     }
 }
 
