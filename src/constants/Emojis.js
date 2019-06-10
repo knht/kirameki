6cc7043cce283e093f644d4834c3b0c0d25ba963
Riya <kato.hana@outlook.jp>
2019-06-10 09:51:52 +0200

    Added all emojis to assets

--- src/constants/Emojis.js
+++ src/constants/Emojis.js
@@ -1,4 +1,76 @@
 module.exports = {
+    OSU: {
+        BEST_PLAYS: {
+            FIRST: '<:yachty_osu_1:451008857242861588>',
+            SECOND: '<:yachty_osu_2:451008857280479262>',
+            THIRD: '<:yachty_osu_3:451008857305513995>'
+        },
+        RANKS: {
+            A: '<:yachty_osu_A:451021069978042379>',
+            B: '<:yachty_osu_B:451021070376501249>',
+            C: '<:yachty_osu_C:451021071131607040>',
+            D: '<:yachty_osu_D:451021071530065920>',
+            F: '<:yachty_osu_F:477882995588595717>',
+            S: '<:yachty_osu_S:451021073555652608>',
+            SS: '<:yachty_osu_X:451021073601921035>',
+            SH: '<:yachty_osu_SH:451021073710841858>',
+            SSH: '<:yachty_osu_XH:451021074004443136>'
+        },
+        HITS: {
+            50: '<:yachty_osu_50:477884733670424588>',
+            100: '<:yachty_osu_100:477884733955506196>',
+            300: '<:yachty_osu_300:477884734048043018>',
+            MISS: '<:yachty_osu_x:477884734328930334>'
+        },
+        DIFFICULTIES: {
+            EASY: '<:yachty_osu_easy:477874057606594561>',
+            EXPERT: '<:yachty_osu_expert:477874058055122944>',
+            EXPERT_PLUS: '<:yachty_osu_expertplus:477874058910760970>',
+            HARD: '<:yachty_osu_hard:477874059330322432>',
+            INSANE: '<:yachty_osu_insane:477874061129809920>',
+            NORMAL: '<:yachty_osu_normal:477874061129809930>'
+        }
+    },
+    LEAGUE_OF_LEGENDS: {
+        TIERS: {
+            BRONZE: '<:yachty_lol_bronze:521082521283198977>',
+            IRON: '<:yachty_lol_iron:521082524370337808>',
+            GOLD: '<:yachty_lol_gold:521082524470870026>',
+            CHALLENGER: '<:yachty_lol_challenger:521082526585061396>',
+            DIAMOND: '<:yachty_lol_diamond:521082530431238169>',
+            PLATINUM: '<:yachty_lol_platinum:521082531853107211>',
+            MASTER: '<:yachty_lol_master:521082531957964801>',
+            SILVER: '<:yachty_lol_silver:521082532024942613>',
+            GRANDMASTER: '<:yachty_lol_grandmaster:521082532427595783>'
+        },
+        MASTERY: {
+            1: '<:yachty_lol_m1:451310694134972418>',
+            2: '<:yachty_lol_m2:451310694235766805>',
+            3: '<:yachty_lol_m3:451310695124959262>',
+            4: '<:yachty_lol_m4:451310696064352266>',
+            5: '<:yachty_lol_m5:451310695640989696>',
+            6: '<:yachty_lol_m6:451310695691059201>',
+            7: '<:yachty_lol_m7:451310696047706113>'
+        },
+        RUNES: {
+            DOMINATION: '<:yachty_lol_8100:452090525189799938>',
+            SORCERY: '<:yachty_lol_8200:452090525198188556>',
+            PRECISION: '<:yachty_lol_8000:452090525269360640>',
+            INSPIRATION: '<:yachty_lol_8300:452090525626138624>',
+            RESOLVE: '<:yachty_lol_8400:452090525705699328>'
+        }
+    },
+    OVERWATCH: {
+        TIERS: {
+            BRONZE: '<:yachty_ov_bronze:450995583671468052>',
+            DIAMOND: '<:yachty_ov_diamond:450997220972429314>',
+            GOLD: '<:yachty_ov_gold:450997221035474944>',
+            SILVER: '<:yachty_ov_silver:450997223640137741>',
+            PLATINUM: '<:yachty_ov_platinum:450997224252506112>',
+            MASTER: '<:yachty_ov_master:450997224713748491>',
+            GRANDMASTER: '<:yachty_ov_grandmaster:450997227230330880>'
+        }
+    },
     REGIONAL_INDICATORS: {
         A: ':a:',
         B: ':b:',
