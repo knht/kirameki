e88388b34cd41a20eb8c4a6ee709c59068283380
Riya <kato.hana@outlook.jp>
2019-06-12 12:17:51 +0200

    Adjusted command stats names

--- src/MessageHandler.js
+++ src/MessageHandler.js
@@ -110,16 +110,15 @@ class MessageHandler {
         timestamps.set(message.author.id, now);
         setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
-        // Execute the command and register statistics in the database
         try {
             command.execute(message, this.kirCore, this.cooldowns);
 
-            const doesCommandExist = await KiramekiHelper.preparedQuery(this.kirCore.DB, 'SELECT commandname FROM command_usage WHERE commandname = ?;', [commandName]);
+            const doesCommandExist = await KiramekiHelper.preparedQuery(this.kirCore.DB, 'SELECT commandname FROM command_usage WHERE commandname = ?;', [command.name]);
 
             if (doesCommandExist.length > 0) {
-                KiramekiHelper.preparedQuery(this.kirCore.DB, 'UPDATE command_usage SET commandcount = commandcount + 1 WHERE commandname = ?;', [commandName]);
+                KiramekiHelper.preparedQuery(this.kirCore.DB, 'UPDATE command_usage SET commandcount = commandcount + 1 WHERE commandname = ?;', [command.name]);
             } else {
-                KiramekiHelper.preparedQuery(this.kirCore.DB, 'INSERT INTO command_usage (id, commandname, commandcount) VALUES (NULL, ?, ?);', [commandName, 1]);
+                KiramekiHelper.preparedQuery(this.kirCore.DB, 'INSERT INTO command_usage (id, commandname, commandcount) VALUES (NULL, ?, ?);', [command.name, 1]);
             }
         } catch (commandExecutionError) {
             KiramekiHelper.log(KiramekiHelper.LogLevel.ERROR, "COMMAND EXECUTION ERROR", `A command couldn't be executed because of: ${commandExecutionError}`);
