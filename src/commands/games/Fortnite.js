b6a1c98740029d0b4e5517ecd44f68ca6cef2341
Riya <kato.hana@outlook.jp>
2019-06-12 11:26:52 +0200

    Fixed spacing issues

--- src/commands/games/Fortnite.js
+++ src/commands/games/Fortnite.js
@@ -1,13 +1,13 @@
 const KiramekiHelper = require('../../KiramekiHelper');
 const KiramekiConfig = require('../../../config/KiramekiConfig');
-const fnClient       = require('fortnite');
+const fnClient = require('fortnite');
 
 class Fortnite {
     constructor() {
         this.name = 'fortnite';
         this.aliases = ['fn'];
         this.cooldown = 5;
-        this.category = KiramekiHelper.categories.GAMES;  
+        this.category = KiramekiHelper.categories.GAMES;
         this.fortnite = new fnClient(KiramekiConfig.fortniteApiKey);
         this.help = {
             message: 'Get up to date stats of a specified Fortnite player.',
@@ -19,7 +19,7 @@ class Fortnite {
 
     async execute(message, kirCore) {
         const [command, ftnLookupUsername] = KiramekiHelper.tailedArgs(message.content, ' ', 1);
-        
+
         if (!ftnLookupUsername) {
             return message.channel.createEmbed(KiramekiHelper.generateHelpEmbed(this.help, this.help.inline));
         }
@@ -28,33 +28,33 @@ class Fortnite {
             const ftnResultset = await this.fortnite.user(ftnLookupUsername, 'pc');
 
             // Solo Stats
-            const ftnUserStatsSoloKD		= ftnResultset.stats.solo.kd;
-            const ftnUserStatsSoloWins		= ftnResultset.stats.solo.wins;
-            const ftnUserStatsSoloKills		= ftnResultset.stats.solo.kills;
-            const ftnUserStatsSoloMatches	= ftnResultset.stats.solo.matches;
-            const ftnUserStatsSoloWinP		= ((ftnUserStatsSoloWins / ftnUserStatsSoloMatches) * 100).toFixed(2) + '%';
+            const ftnUserStatsSoloKD = ftnResultset.stats.solo.kd;
+            const ftnUserStatsSoloWins = ftnResultset.stats.solo.wins;
+            const ftnUserStatsSoloKills = ftnResultset.stats.solo.kills;
+            const ftnUserStatsSoloMatches = ftnResultset.stats.solo.matches;
+            const ftnUserStatsSoloWinP = ((ftnUserStatsSoloWins / ftnUserStatsSoloMatches) * 100).toFixed(2) + '%';
 
             // Duo Stats
-            const ftnUserStatsDuoKD			= ftnResultset.stats.duo.kd;
-            const ftnUserStatsDuoWins		= ftnResultset.stats.duo.wins;
-            const ftnUserStatsDuoKills		= ftnResultset.stats.duo.kills;
-            const ftnUserStatsDuoMatches	= ftnResultset.stats.duo.matches;
-            const ftnUserStatsDuoWinP		= ((ftnUserStatsDuoWins / ftnUserStatsDuoMatches) * 100).toFixed(2) + '%';
+            const ftnUserStatsDuoKD = ftnResultset.stats.duo.kd;
+            const ftnUserStatsDuoWins = ftnResultset.stats.duo.wins;
+            const ftnUserStatsDuoKills = ftnResultset.stats.duo.kills;
+            const ftnUserStatsDuoMatches = ftnResultset.stats.duo.matches;
+            const ftnUserStatsDuoWinP = ((ftnUserStatsDuoWins / ftnUserStatsDuoMatches) * 100).toFixed(2) + '%';
 
             // Squad Stats
-            const ftnUserStatsSquadKD		= ftnResultset.stats.squad.kd;
-            const ftnUserStatsSquadWins		= ftnResultset.stats.squad.wins;
-            const ftnUserStatsSquadKills	= ftnResultset.stats.squad.kills;
-            const ftnUserStatsSquadMatches	= ftnResultset.stats.squad.matches;
-            const ftnUserStatsSquadWinP		= ((ftnUserStatsSquadWins / ftnUserStatsSquadMatches) * 100).toFixed(2) + '%';
+            const ftnUserStatsSquadKD = ftnResultset.stats.squad.kd;
+            const ftnUserStatsSquadWins = ftnResultset.stats.squad.wins;
+            const ftnUserStatsSquadKills = ftnResultset.stats.squad.kills;
+            const ftnUserStatsSquadMatches = ftnResultset.stats.squad.matches;
+            const ftnUserStatsSquadWinP = ((ftnUserStatsSquadWins / ftnUserStatsSquadMatches) * 100).toFixed(2) + '%';
 
             // User Stats
-            const ftnUsername				= ftnResultset.username;
-            const ftnUserMatchesAll			= ftnUserStatsSoloMatches 	+ ftnUserStatsDuoMatches 	+ ftnUserStatsSquadMatches;
-            const ftnUserKillsAll			= ftnUserStatsSoloKills		+ ftnUserStatsDuoKills		+ ftnUserStatsSquadKills;
-            const ftnUserKDAll				= ftnResultset.stats.lifetime.kd;
-            const ftnUserWinsAll			= ftnResultset.stats.lifetime.wins;
-            const ftnUserWinPAll			= ((ftnUserWinsAll / ftnUserMatchesAll) * 100).toFixed(2) + '%';
+            const ftnUsername = ftnResultset.username;
+            const ftnUserMatchesAll = ftnUserStatsSoloMatches + ftnUserStatsDuoMatches + ftnUserStatsSquadMatches;
+            const ftnUserKillsAll = ftnUserStatsSoloKills + ftnUserStatsDuoKills + ftnUserStatsSquadKills;
+            const ftnUserKDAll = ftnResultset.stats.lifetime.kd;
+            const ftnUserWinsAll = ftnResultset.stats.lifetime.wins;
+            const ftnUserWinPAll = ((ftnUserWinsAll / ftnUserMatchesAll) * 100).toFixed(2) + '%';
 
             message.channel.createEmbed(new KiramekiHelper.Embed()
                 .setColor('DEFAULT')
@@ -67,38 +67,38 @@ class Fortnite {
                 .addBlankField(false)
                 .addField(
                     'Solo Stats',
-                    `**K/D:** ${ftnUserStatsSoloKD}\n` + 
+                    `**K/D:** ${ftnUserStatsSoloKD}\n` +
                     `**Wins:** ${ftnUserStatsSoloWins}\n` +
-                    `**Kills:** ${ftnUserStatsSoloKills}\n` + 
-                    `**Winrate:** ${ftnUserStatsSoloWinP}\n` + 
+                    `**Kills:** ${ftnUserStatsSoloKills}\n` +
+                    `**Winrate:** ${ftnUserStatsSoloWinP}\n` +
                     `**Matches:** ${ftnUserStatsSoloMatches}`,
                     true
                 )
                 .addField(
                     'Duo Stats',
-                    `**K/D:** ${ftnUserStatsDuoKD}\n` + 
+                    `**K/D:** ${ftnUserStatsDuoKD}\n` +
                     `**Wins:** ${ftnUserStatsDuoWins}\n` +
-                    `**Kills:** ${ftnUserStatsDuoKills}\n` + 
-                    `**Winrate:** ${ftnUserStatsDuoWinP}\n` + 
+                    `**Kills:** ${ftnUserStatsDuoKills}\n` +
+                    `**Winrate:** ${ftnUserStatsDuoWinP}\n` +
                     `**Matches:** ${ftnUserStatsDuoMatches}`,
                     true
                 )
                 .addBlankField(false)
                 .addField(
                     'Squad Stats',
-                    `**K/D:** ${ftnUserStatsSquadKD}\n` + 
+                    `**K/D:** ${ftnUserStatsSquadKD}\n` +
                     `**Wins:** ${ftnUserStatsSquadWins}\n` +
-                    `**Kills:** ${ftnUserStatsSquadKills}\n` + 
-                    `**Winrate:** ${ftnUserStatsSquadWinP}\n` + 
+                    `**Kills:** ${ftnUserStatsSquadKills}\n` +
+                    `**Winrate:** ${ftnUserStatsSquadWinP}\n` +
                     `**Matches:** ${ftnUserStatsSquadMatches}`,
                     true
                 )
                 .addField(
                     'Lifetime Stats',
-                    `**K/D:** ${ftnUserKDAll}\n` + 
+                    `**K/D:** ${ftnUserKDAll}\n` +
                     `**Wins:** ${ftnUserWinsAll}\n` +
-                    `**Kills:** ${ftnUserKillsAll}\n` + 
-                    `**Winrate:** ${ftnUserWinPAll}\n` + 
+                    `**Kills:** ${ftnUserKillsAll}\n` +
+                    `**Winrate:** ${ftnUserWinPAll}\n` +
                     `**Matches:** ${ftnUserMatchesAll}`,
                     true
                 )
