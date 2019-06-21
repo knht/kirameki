fb5e9a121deb70edc983211a123962736f67cc6f
Riya <kato.hana@outlook.jp>
2019-06-21 17:01:48 +0200

    Added Summoner Spec barebones command structure

---
+++ src/commands/games/SummonerSpec.js
@@ -0,0 +1,41 @@
+const KiramekiHelper = require('../../KiramekiHelper');
+
+class SummonerSpec {
+    constructor() {
+        this.name = 'leaguespec';
+        this.aliases = ['summonerspec', 'summoners', 'summonerspectate', 'leaguespectate'];
+        this.permissions = ['externalEmojis'];
+        this.category = KiramekiHelper.categories.GAMES;
+        this.cooldown = 5;
+        this.help = {
+            message: 'Get live spectator game data about any player in League of Legends currently in-game.',
+            usage: 'leaguespec <server> <summonerName>',
+            example:  ['leaguespec euw Bjergsen', 'leaguespec kr Faker'],
+            inline: false
+        }
+    }
+
+    async execute(message, kirCore) {
+        const [command, region, summoner] = KiramekiHelper.tailedArgs(message.content, ' ', 2);
+        const server = KiramekiHelper.other.LEAGUE_OF_LEGENDS.REGION.getServerByRegion(region);
+
+        if (!region || !summoner) {
+            return message.channel.createEmbed(KiramekiHelper.generateHelpEmbed(this.help, this.help.inline));
+        }
+
+        if (region && !server) {
+            return message.channel.createEmbed(new KiramekiHelper.Embed()
+                .setColor('RED')
+                .setTitle(`There is no supported server region called **${region}**!`)
+            );
+        }
+
+        /**
+         * @todo Find workaround for missing Riot API endpoints and rewrite all of the League of Legends commands.
+         */
+
+        KiramekiHelper.log(KiramekiHelper.LogLevel.COMMAND, 'SUMMONER SPEC', `${KiramekiHelper.userLogCompiler(message.author)} used the ${this.name} command.`);
+    }
+}
+
+module.exports = new SummonerSpec();
