d8fa3fd407ef559f417f928bc5f3015dd33f2477
Riya <kato.hana@outlook.jp>
2019-07-04 16:16:29 +0200

    Added a few paginator options

--- src/extensions/PaginationEmbed.js
+++ src/extensions/PaginationEmbed.js
@@ -5,24 +5,39 @@ class PaginationEmbed {
         this.pages = pages;
         this.invoker = message;
         this.options = options;
-        this.back = '⬅';
-        this.forth = '➡';
-        this.page = 1;
+        this.back = options.backButton || '⬅';
+        this.forth = options.forthButton || '➡';
+        this.page = options.startPage || 1;
+        this.maxMatches = options.maxMatches || 50;
+        this.timeout = options.timeout || 300000;
+        this.showPageNumbers = (typeof options.showPageNumbers !== 'undefined') ? options.showPageNumbers : true;
         this.message;
         this.handler;
     }
 
     async initialize() {
         if (this.pages.length < 2) {
-            return Promise.reject('PaginationEmbedError: A Pagination Embed must contain at least 2 pages!');
+            return Promise.reject(new Error('A Pagination Embed must contain at least 2 pages!'));
         }
 
+        if (this.page < 1 || this.page > this.pages.length) {
+            return Promise.reject(new Error(`Invalid start page! Must be between 1 (first) and ${this.pages.length} (last)`));
+        }
+
+        if (this.maxMatches > 100) {
+            return Promise.reject(new Error('Maximum amount of page changes exceeded! Must be under 100!'));
+        }
+
+        if (this.timeout > 900000) {
+            return Promise.reject(new Error('Embed Timeout too high! Maximum pagination lifespan allowed is 15 minutes (900000 ms)!'));
+        }
+        
         this.message = await this.invoker.channel.createMessage({
-            content: `Page **${this.page}** of **${this.pages.length}**`,
+            content: (this.showPageNumbers) ? `Page **${this.page}** of **${this.pages.length}**` : undefined,
             embed: this.pages[this.page - 1]
         });
 
-        this.handler = new ReactionHandler.continuousReactionStream(this.message, (userID) => userID === this.invoker.author.id, { maxMatches: 100, time: 300000 });
+        this.handler = new ReactionHandler.continuousReactionStream(this.message, (userID) => userID === this.invoker.author.id, { maxMatches: this.maxMatches, time: this.timeout });
 
         await this.message.addReaction(this.back);
         await this.message.addReaction(this.forth);
@@ -37,7 +52,7 @@ class PaginationEmbed {
                     if (this.page > 1) {
                         this.page--;
                         this.message.edit({
-                            content: `Page **${this.page}** of **${this.pages.length}**`,
+                            content: (this.showPageNumbers) ? `Page **${this.page}** of **${this.pages.length}**` : undefined,
                             embed: this.pages[this.page - 1]
                         });
                     }
@@ -51,7 +66,7 @@ class PaginationEmbed {
                     if (this.page < this.pages.length) {
                         this.page++;
                         this.message.edit({
-                            content: `Page **${this.page}** of **${this.pages.length}**`,
+                            content: (this.showPageNumbers) ? `Page **${this.page}** of **${this.pages.length}**` : undefined,
                             embed: this.pages[this.page - 1]
                         });
                     }
@@ -63,8 +78,8 @@ class PaginationEmbed {
     }
 }
 
-module.exports = async (message, pages) => {
-    const paginationEmbed = new PaginationEmbed(message, pages);
+module.exports = async (message, pages, options) => {
+    const paginationEmbed = new PaginationEmbed(message, pages, options);
     await paginationEmbed.initialize();
     paginationEmbed.run();
 };
