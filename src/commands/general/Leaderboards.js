64ff1ea00a0e93d7f0cf2be175bdc8fccca9fc7c
Riya <kato.hana@outlook.jp>
2019-11-14 21:43:24 +0100

    Added server XP leaderboards

--- src/commands/general/Leaderboards.js
+++ src/commands/general/Leaderboards.js
@@ -6,9 +6,9 @@ class Leaderboards {
         this.aliases = ['leaderboards'];
         this.category = KiramekiHelper.categories.GENERAL;
         this.help = {
-            message: 'Access all the different kinds of leaderboards available on Kirameki.\nCurrently available leaderboards: `osu`, `xp`',
+            message: 'Access all the different kinds of leaderboards available on Kirameki.\nCurrently available leaderboards: `osu`, `xp`, `guild` or `server`',
             usage: 'leaderboard <board>',
-            example: ['leaderboard xp', 'leaderboard osu'],
+            example: ['leaderboard xp', 'leaderboard osu', 'leaderboard guild'],
             inline: false
         }
     }
@@ -19,6 +19,50 @@ class Leaderboards {
         if (!leaderboard) return message.channel.createEmbed(KiramekiHelper.generateHelpEmbed(this.help));
 
         switch (leaderboard.toLowerCase()) {
+            case 'guild': 
+            case 'server': {
+                const activeMemberIDs = message.channel.guild.members.map(guildMember => `'${guildMember.id}'`);
+                const query = `SELECT * FROM profile_xp WHERE discord_id IN (${activeMemberIDs.join(', ')}) ORDER BY xp DESC LIMIT 10;`;
+                const result = await KiramekiHelper.query(kirCore.DB, query);
+                const userProfile = result.filter(profile => profile.discord_id == message.author.id)[0];
+                const topTen = [];
+
+                for (let i = 0; i < result.length; i++) {
+                    switch (i) {
+                        case 0: {
+                            topTen.push(`${KiramekiHelper.emojis.OSU.BEST_PLAYS.FIRST} **${result[i].discord_tag}** is Level **${result[i].level}** and has **${result[i].pats}** Pats.`);
+                            break;
+                        }
+
+                        case 1: {
+                            topTen.push(`${KiramekiHelper.emojis.OSU.BEST_PLAYS.SECOND} **${result[i].discord_tag}** is Level **${result[i].level}** and has **${result[i].pats}** Pats.`);
+                            break;
+                        }
+
+                        case 2: {
+                            topTen.push(`${KiramekiHelper.emojis.OSU.BEST_PLAYS.THIRD} **${result[i].discord_tag}** is Level **${result[i].level}** and has **${result[i].pats}** Pats.`);
+                            break;
+                        }
+
+                        default: {
+                            topTen.push(`➖ **${result[i].discord_tag}** is Level **${result[i].level}** and has **${result[i].pats}** Pats.`);
+                            break;
+                        }
+                    }
+                }
+
+                message.channel.createEmbed(new KiramekiHelper.Embed()
+                    .setColor('DEFAULT')
+                    .setTitle(`Experience Leaderboards for **__${message.channel.guild.name}__**`)
+                    .setThumbnail(message.channel.guild.iconURL)
+                    .setDescription(`Currently **${result[0].discord_tag}** is leading in guild **${message.channel.guild.name}** with **${KiramekiHelper.numberWithCommas(result[0].xp)} XP**!`)
+                    .addField(`${message.channel.guild.name}'s Top 10 Most Active Users`, topTen.join('\n'))
+                    .setFooter(`${userProfile.discord_tag} • Level ${userProfile.level} • ${userProfile.xp} XP • ${userProfile.pats} Pats`, message.author.dynamicAvatarURL('jpg', 16))
+                );
+
+                break;
+            }
+
             case 'xp': {
                 message.channel.createEmbed(new KiramekiHelper.Embed()
                     .setColor('DEFAULT')
