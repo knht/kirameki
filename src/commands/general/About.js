0eb348b79fb3d212ed5b0b678824707ccb49bef4
Riya <kato.hana@outlook.jp>
2020-02-01 00:42:12 +0100

    Updated source code URL to be pulling from constants file

--- src/commands/general/About.js
+++ src/commands/general/About.js
@@ -31,7 +31,7 @@ class About {
             .addField("Dashboard", `[Click Here](${KiramekiHelper.links.WEBSITE.DASHBOARD})`, true)
             .addField("Discord Server", `[Kirameki Help Guild](${KiramekiHelper.links.INVITE})`, true)
             .addField("Invite Kirameki", `[Click Here](${KiramekiHelper.links.WEBSITE.INVITE})`, true)
-            .addField("Source", `[GitHub](https://github.com/riyacchi/kirameki)`, true)
+            .addField("Source", `[GitHub](${KiramekiHelper.links.SOURCE.REPOSITORY.GITHUB})`, true)
             .setThumbnail(KiramekiHelper.images.KIRAMEKI_MASCOT)
             .setFooter("Rocking 451 libraries, 19 APIs and a lot of love ❤️")
         );
